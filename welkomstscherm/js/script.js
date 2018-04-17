fbc756d8ebb2fc90ee8f34dd8bdc6c33b2ed843d
Niels <evniels95@gmail.com>
2018-04-17 15:32:50 +0200

    further changes

--- welkomstscherm/js/script.js
+++
@@ -1,4 +0,0 @@
- var x = document.getElementsByTagName("BODY")[0];
- x.onkeydown = function(){
-   window.location.replace("file:///Users/niels/Documents/GitHub/hifi_prototype_f/laadpagina/index.html");
- }
